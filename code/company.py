dd3d768bb6ce282478de34caf1e0ef244d824d7a
zemota1 <zemariamota@hotmail.com>
2018-05-11 15:59:24 +0100

    company local

--- code/company.py
+++ code/company.py
@@ -1,9 +1,12 @@
+import truck
+
 class Company:
 
-    def __init__(self, id, list_trucks):
+    def __init__(self, id, list_trucks, budget, local):
         self.id = id
         self.trucks = list_trucks
-        self.profit = 0
+        self.profit = budget
+        self.local = local
 
     def getID(self):
         return self.id
@@ -17,8 +20,14 @@ class Company:
     def getProfit(self):
         return self.profit
 
-    def setProfit(self, profit):
-        self.profit = profit
+    def setProfit(self, budget):
+        self.profit = budget
+
+    def getLocal(self):
+        return self.local
+
+    def setLocal(self, local):
+        self.local = local
 
     def evaluateOffer(self, offer):
         start = offer[0]
@@ -28,3 +37,18 @@ class Company:
 
     def getUtility(money, distance):
         return money * distance
+
+    def buyTrucks(self, policy):
+
+    #[1,2,3,4] 1: curto pessoas, 2: curto mercadorias, 3: longo pessoas, 4: longo mercadorias
+        if policy == 1:
+            x = truck.FiftyBus()
+        elif policy == 2:
+            x = truck.FiftyTruck()
+        elif policy == 3:
+            x = truck.SeventyBus()
+        elif policy == 4:
+            x = truck.SeventyTruck()
+
+        self.profit -= x.getPrice()
+        addTruck(x)
